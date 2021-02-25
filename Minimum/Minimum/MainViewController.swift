69c8c8e76e9415a07d5bcaf9167dad583a021484
wonyoungpark94 <parkwonyoung94@gmail.com>
2021-02-25 15:28:36 +0900

    storyboard_setup

---
+++ Minimum/Minimum/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minimum
+//
+//  Created by park wonyoung on 2021/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
