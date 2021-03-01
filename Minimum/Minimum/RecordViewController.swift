9f410a202d92beb2c5a686f58a840b0f5431a1ec
wonyoungpark94 <parkwonyoung94@gmail.com>
2021-03-02 02:36:21 +0900

    record date, weight and memo

--- Minimum/Minimum/RecordViewController.swift
+++ Minimum/Minimum/RecordViewController.swift
@@ -7,23 +7,93 @@
 
 import UIKit
 
-class RecordViewController: UIViewController {
-
+class RecordViewController: UIViewController, UITextFieldDelegate {
+    
+    //날짜//
+    @IBOutlet weak var recordedDate: UIButton!
+    @IBOutlet weak var selectDatePicker: UIDatePicker!
+    
+    
+    //메모//
+    @IBOutlet weak var memoTextField: UITextField!
+    @IBOutlet weak var countCharacterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //날짜//
+        loadDate()
+        
+        //날짜//DatePicker 기본 설정
+        selectDatePicker.datePickerMode = .date
+        selectDatePicker.preferredDatePickerStyle = .wheels
+        selectDatePicker.isHidden = true
+        
+        //메모//
+        memoTextField.delegate = self
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //날짜// view load시 현재 날짜 자동 입력
+    func loadDate() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM월 dd일, 20YY"
+        
+        let dateString = formatter.string(from: date)
+        recordedDate.setTitle(dateString, for: .normal)
     }
-    */
+    
+    //날짜// 클릭 시 하단에서 pickerView 보이기&숨기기
+    @IBAction func showDateButton(_ sender: Any) {
+        if selectDatePicker.isHidden == false {
+            selectDatePicker.isHidden = true
+        } else {
+            selectDatePicker.isHidden = false
+         }
+    }
+
+    //날짜// datePicekr 조정하면 String 바꾸기
+    @IBAction func selectDateAction(_ sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM월 dd일, 20YY"
+        
+        let dataString = formatter.string(from: sender.date)
+        recordedDate.setTitle(dataString, for: .normal)
+    }
+    
+    //날짜// view 아무데나 누르면 pikcerView 사라짐
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        selectDatePicker.isHidden = true
+    }
+    
+    //메모// 글자 수 제한
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let limitLength = 60
+        
+            guard let text = textField.text else { return true }
+            let newLength = text.count + string.count - range.length
+        
+            //print(text.count)
+            countCharacterLabel.text = "\(text.count + 1) / 60"
+        
+            return newLength < limitLength
+    }
+    
+    
+    
+    
+    
+
+    //완료//
+    @IBAction func completeButtonTapped(_ sender: Any) {
+        print("완료 버튼이 눌렸습니다.")
+        
+        //화면 이동
+        self.navigationController?.popViewController(animated: true)
+        
+        //data 저장
+    }
 
 }
---
+++ Minimum/build/Pods.build/Debug-iphonesimulator/Pods-Minimum.build/Objects-normal/x86_64/Pods_Minimum_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/parkwonyoung/Documents/GitHub/Minimum_Front/Minimum/build/Pods.build/Debug-iphonesimulator/Pods-Minimum.build/DerivedSources/Pods_Minimum_vers.c
