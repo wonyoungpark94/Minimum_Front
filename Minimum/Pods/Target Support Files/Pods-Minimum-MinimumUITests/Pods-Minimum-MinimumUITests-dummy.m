982f1ecc07b731afb5b424fde982d721c1593d51
wonyoungpark94 <parkwonyoung94@gmail.com>
2021-03-22 16:13:53 +0900

    pod update

--- Minimum/Pods/Target Support Files/Pods-Minimum-MinimumUITests/Pods-Minimum-MinimumUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Minimum_MinimumUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Minimum_MinimumUITests
-@end
--- Minimum/Pods/Target Support Files/Pods-MinimumTests/Pods-MinimumTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MinimumTests : NSObject
-@end
-@implementation PodsDummy_Pods_MinimumTests
-@end
