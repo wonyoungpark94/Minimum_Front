8adffe05d86e7e2d1314323100b8b1fecc7d9cbc
wonyoungpark94 <parkwonyoung94@gmail.com>
2021-03-30 13:06:21 +0900

    historyVC auto layout

--- Minimum/Minimum/HistoryVC/HistoryViewController.swift
+++ Minimum/Minimum/HistoryVC/HistoryViewController.swift
@@ -24,11 +24,11 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     //더미데이터 test
     let dataList = [
-        CellData(date: "2021년 1월 21일 오전 10시 17분", weight: "1일 전과 비교해서 +0.2kg", memo: "어제 고은이랑 떡볶이를 먹었다. 내가 미쳤지", image: nil),
-        CellData(date: "2021년 1월 20일 오전 10시 01분", weight: "1일 전과 비교해서 -0.3kg", memo: "야식을 안 먹고 잤더니 몸이 가볍네", image: nil),
-        CellData(date: "2021년 1월 19일 오전 09시 52분", weight: "1일 전과 비교해서 -0.2kg", memo: "어제 운동 빡시게 함!!!", image: nil),
-        CellData(date: "2021년 1월 18일 오전 10시 02분", weight: "2일 전과 비교해서 -0.0kg", memo: "며칠 째 그대로네", image: nil),
-        CellData(date: "2021년 1월 16일 오전 09시 52분", weight: "1일 전과 비교해서 -0.0kg", memo: "운동하고나서 찜닭먹었더니 ㅜㅜ", image: nil)
+        CellData(date: "2021년 1월 21일 오전 10시 17분", weight: "1일 전과 비교해서", memo: "어제 고은이랑 떡볶이를 먹었다. 내가 미쳤지", image: nil),
+        CellData(date: "2021년 1월 20일 오전 10시 01분", weight: "1일 전과 비교해서", memo: "야식을 안 먹고 잤더니 몸이 가볍네", image: nil),
+        CellData(date: "2021년 1월 19일 오전 09시 52분", weight: "1일 전과 비교해서", memo: "어제 운동 빡시게 함!!!", image: nil),
+        CellData(date: "2021년 1월 18일 오전 10시 02분", weight: "2일 전과 비교해서", memo: "며칠 째 그대로네", image: nil),
+        CellData(date: "2021년 1월 16일 오전 09시 52분", weight: "1일 전과 비교해서", memo: "운동하고나서 찜닭먹었더니 ㅜㅜ", image: nil)
     ]
     
     override func viewDidLoad() {
